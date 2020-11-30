d5a85c47ee22fceb609dbe16c808b6876bc8c463
Dennis Koech <koechdennis031@gmail.com>
2020-11-30 20:37:36 +0300

    Added The admin login, functionalities to be added further on down the line

--- borderlinebloggers/app/Http/Controllers/Auth/LoginController.php
+++ borderlinebloggers/app/Http/Controllers/Auth/LoginController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-
+use Illuminate\Support\Facades\Auth;
 class LoginController extends Controller
 {
     /*
@@ -26,7 +26,13 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected function redirectTo(){
+        if(Auth::user()->usertype == 'admin'){
+            return 'master';
+        }else{
+            return 'home';
+        }
+    }
 
     /**
      * Create a new controller instance.
--- borderlinebloggers/routes/web.php
+++ borderlinebloggers/routes/web.php
@@ -20,3 +20,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/master', function(){
+    return view('layouts.master');
+});
+Route::group(['middleware'=>['auth','admin']], function(){
+    Route::get('/dashboard', function(){
+        return view('layouts.master');
+    });
+});
