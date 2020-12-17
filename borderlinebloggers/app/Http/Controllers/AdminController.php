10f3cd129ed9dd1414f3c54eb4304a00970bc6fd
Tracy Ogutu <tracyogutu@gmail.com>
2020-12-17 12:34:34 +0300

    add user list function

--- borderlinebloggers/app/Http/Controllers/AdminController.php
+++ borderlinebloggers/app/Http/Controllers/AdminController.php
@@ -7,4 +7,12 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     //
+    public function __construct(){
+        $this->middleware('auth'); 
+    }
+    public function user_list(){
+        $users = User::all();
+        return view('admin.list_user')->with('users',$users);
+    }
+    
 }
